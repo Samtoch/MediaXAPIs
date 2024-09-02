b9e1ed87630a6081df95b444db9d81f8e9acddde
tochisam <samueltochi1@gmail.com>
2024-09-02 23:06:38 +0100

    Implemetation of Image Url

--- MediaXAPIs/obj/Debug/net8.0/MediaXAPIs.AssemblyInfo.cs
+++ MediaXAPIs/obj/Debug/net8.0/MediaXAPIs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediaXAPIs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c0c996845dd0e07c3db6b1e22ca747cecda6408")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3160e7eb14ec8be0f51bd1afe0787838820bbfa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediaXAPIs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediaXAPIs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
